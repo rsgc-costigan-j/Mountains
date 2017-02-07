b646b0f4ca906c77e8ab86def4fca91d8ba1157f
Kathleen McLaughlin <kathleen.mclaughlin.17@gmail.com>
2017-02-07 13:04:36 -0500

    added instantiation of variable

--- Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -16,16 +16,30 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Draw some lines
-
 //dots
 for y in stride(from: 300, through: 0, by: -25){
     for x in stride(from: 0, through: 300, by: 25){
         canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
     }
 }
-// Draw the dark shape
 
+// Draw the dark shape
+var x: int
+var y: int
+var a: int
+var b: int
+var x: int
+var x: int
+x = 0
+y = 8
+a = 0
+b = 4
+z = 0
+f = 0
+
+while ( z = 0) {
+    
+}
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
